86b7e9d4e5a6fffa0f655965628a34f78f0873c1
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2022-12-08 23:34:15 +0600

    Questions with answers

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("My favorite color?: ______");
+        System.out.println("Name: Syrkhan");
+        System.out.println("Surname: Madiyev");
+        System.out.println("Id: 210103211");
+        System.out.println("Hobby: Climbing on mountains");
+        System.out.println("How much do you like ICT(from 1-10)): 9");
+        System.out.println("Rate this task from 1 to 10: 3");
+        System.out.println("My favorite color?: pink");
     }
 }
